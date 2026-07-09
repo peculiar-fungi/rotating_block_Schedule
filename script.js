4756991bd2badfdc8cfdc161d83f0ff5b51f6c27
rohit sudhakaran <rhtsudhakaran@gmail.com>
2026-07-09 19:54:47 -0400

    made dark mode default

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const button = document.getElementById('actionBtn');
     const greeting = document.getElementById('greeting');
     const body = document.body;
-
+    body.classList.toggle('dark-mode');
     button.addEventListener('click', () => {
         // Toggle dark mode class on the body
         body.classList.toggle('dark-mode');
