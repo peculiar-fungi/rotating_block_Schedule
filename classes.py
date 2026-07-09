b7e8529709b51bc4f0a09f08a094de6273636944
rohit sudhakaran <rhtsudhakaran@gmail.com>
2026-07-09 15:19:25 -0400

    first

---
+++ classes.py
@@ -0,0 +1,10 @@
+lu="Lunch"
+lab="lab"
+h="Homeroom"
+a_day=[1,h,2,3,lab,lu,lab,5,6,7]
+b_day=[4,h,1,2,lab,lu,lab,8,5,6]
+c_day=[3,h,4,1,lab,lu,lab,7,8,5]
+d_day=[2,h,3,4,lab,lu,lab,6,7,8]
+e_day=[1,2,3,4,lu,5,6,7,8]
+ed_e_day=[1,2,3,4,5,6,7,8]
+
